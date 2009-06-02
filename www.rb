fe92586cb11897022d3d939f130ee715dd7207cd
nsussman <nsussman@NYCD887.bnweb.user.bn>
2009-06-02 11:58:37 -0400

    web service framework enables executing functions that call curl, in a loop.  Results can either be written to a file (with time stamps) or echoed to the shell.

--- www.rb
+++ www.rb
@@ -1,17 +1,53 @@
-# logbot
+# :title:WWW.rb
+# = Name
+# WWW.rb
+# = Synopsis
+#     require 'www.rb'
+#     log 1, "log.txt", lambda{`curl -s http://localhost`}
+# = Description
+# A specialized, minimal Web service framework in Ruby.
 
-# pollbot
-#     poll 1, lambda{"foo"}   
+require 'log-util.rb'
+
+# == logbot
+#
+# writes the result of fn to file
+#
+#     log 1, "log.txt", lambda{`curl -s http://localhost`}
+
+def log delay, file, fn
+
+  bot delay, lambda{format_log_entry file, fn.call()}
+
+end
+
+# == pollbot
+#
+#     poll 1, lambda{puts `curl -s http://localhost`}
+#
+# note that puts was needed in the lambda function to print the result
+# of the poll, but is not needed for the logger
 
 def poll delay, fn
 
+  bot delay, fn
+
+end
+
+# bot factory
+
+def bot delay, fn
+
   while true do
-    #puts "ping"
+
     fn.call()
     sleep delay
-    poll delay, fn    
+    bot delay, fn    
+
   end
 
 
 end
-c
+
+
+
