43d519db8a20ff9bc4beb0e92c2f4552b23adbde
nsussman <nsussman@NYCD887.bnweb.user.bn>
2009-06-02 11:58:54 -0400

    minimal utility kit for tiny web service

--- log-util.rb
+++ log-util.rb
@@ -8,3 +8,13 @@ def get_timestamp
 
 end
 
+def format_log_entry file, entry_body
+
+  str = get_timestamp
+  str << "\n"
+  str << entry_body
+  str << "\n\n"
+
+  File.open(file, 'a') {|f| f.write(str) }
+
+end
