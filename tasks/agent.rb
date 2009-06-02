12ea13c7bb04d0ca896f8839985c0d58eec532ba
nsussman <nsussman@NYCD887.bnweb.user.bn>
2009-06-02 12:22:32 -0400

    example rake task for starting the logger and running tests, for bare-bones continuous integration

---
+++ tasks/agent.rb
@@ -0,0 +1,27 @@
+# :title:Test Agent tasks
+# = Name
+# Test Agent tasks
+# = Synopsis
+#
+#     # in your rakefile
+#     require 'tasks/agent.rb'
+#
+# = Description
+#
+# Start a logging process that runs all the tests and publishes the
+# result to a log file.  Then publish the last entry in the log file
+# to a remote (HTML) file, overwriting the old published version.
+
+namespace :agent do
+
+  require 'www.rb'
+
+  desc "publish all test results to the log every half hour"
+
+  task :monitor do
+
+    log 0.5*60, "log/agent.txt", lambda{`rake test`}
+
+  end
+
+end
